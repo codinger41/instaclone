04c7fc11717c6ce61ba832e054027844e204773f
leksyib <leksyib14@gmail.com>
2017-10-17 15:09:41 +0100

    smh

--- server.js
+++ server.js
@@ -15,36 +15,11 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Static files
+
 app.use(express.static(__dirname + '/dist'));
 
 
-const imageList = [
-	{
-		key: 0,
-		url: "https://process.filestackapi.com/sharpen/negative/sb5RRdoQiiy5l5JUglB1"
-	},
-	{
-		key: 1,
-		url: "https://process.filestackapi.com/sharpen/oil_paint/urjTyRrAQA6sUzK2qIsd"
-	},
-	{
-		key: 2,
-		url: "https://process.filestackapi.com/sepia/modulate/wxYyL4yQyyRH1RQLZ6gL"
-	},
-	{
-		key: 3,
-		url: "https://process.filestackapi.com/blur/pixelate/O9vo0AynTNaNZlRyRBUm"
-	},
-	{
-		key: 4,
-		url: "https://process.filestackapi.com/blackwhite/kcirovLQC2eJmA6pkrMD"
-	},
-	{
-		key: 5,
-		url: "https://process.filestackapi.com/sharpen/modulate/5V2ZH22ZTWGXv2lMvvVT"
-	}
-];
+
 
 app.route('/image')
 	.get((req, res) => res.json(imageList))
